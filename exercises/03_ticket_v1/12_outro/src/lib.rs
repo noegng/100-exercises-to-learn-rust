e2dc93a0809a9574ba65ba2137593d945a529716
noegng <noe.guenego@gmail.com>
2026-09-17 11:30:33 +0200

    session 2

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{fmt::format, ptr::null};
+
 // TODO: Define a new `Order` type.
 //   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
 //   The product name can't be empty and it can't be longer than 300 bytes.
@@ -11,3 +13,74 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+pub struct Order{
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32 ) -> Order {
+        validate_name(&product_name);
+        validate_quantity(quantity);
+        validate_price(unit_price);
+
+    Order {
+        product_name,
+        quantity,
+        unit_price,
+        }
+    }
+
+pub fn product_name(&self) -> &String{
+    &self.product_name
+}
+
+pub fn quantity(&self) -> &i32 {
+    &self.quantity
+}
+
+pub fn unit_price(&self) -> &i32 {
+    &self.unit_price
+}
+
+pub fn set_product_name(&mut self, new_name: String){
+    validate_name(&new_name);
+    self.product_name = new_name;
+}
+pub fn set_quantity(&mut self, new_quantity: i32){
+    validate_quantity(new_quantity);
+    self.quantity = new_quantity;
+}
+
+pub fn set_unit_price(&mut self, new_unit_price: i32){
+    validate_price(new_unit_price);
+    self.unit_price = new_unit_price;
+}
+
+pub fn total(&self) -> i32 {
+let total: i32 = &self.quantity * &self.unit_price;
+return total;
+}
+
+
+}
+
+fn validate_name(product_name: &String){
+if (product_name.is_empty() || product_name.len() > 300){
+    panic!("Product name can't be empty and can't be over 300 bytes")
+    }
+}
+
+fn validate_quantity(quantity: i32){
+    if (quantity <= 0 ){
+        panic!("Quantity can't be negative or 0")
+    }
+}
+
+fn validate_price(unit_price: i32){
+    if (unit_price <= 0){
+        panic!("Unit price must be strictly greater than 0!")
+    }
+}
+
